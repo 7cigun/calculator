186cf0d370a0e44f50fe26ecb16793c0e8643621
7cigun <7cigun@gmail.com>
2022-02-03 19:48:53 +0300

    Добавил listener для кнопок.

--- app/src/main/java/ru/gb/calculator/MainActivity.java
+++ app/src/main/java/ru/gb/calculator/MainActivity.java
@@ -3,12 +3,83 @@ package ru.gb.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonOne;
+    private Button buttonTwo;
+    private Button buttonThree;
+    private Button buttonFour;
+    private Button buttonFive;
+    private Button buttonSix;
+    private Button buttonSeven;
+    private Button buttonEight;
+    private Button buttonNine;
+    private Button buttonZero;
+    private Button buttonPlus;
+    private Button buttonMinus;
+    private Button buttonDivision;
+    private Button buttonMultiplication;
+    private Button buttonPoint;
+    private Button buttonCount;
+
+
+    private TextView calculatorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        initView();
+        setListeners();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        calculatorTextView.setText(String.format("%s%s", calculatorTextView.getText().toString(), button.getText().toString()));
+    }
+
+    private void setListeners() {
+        buttonOne.setOnClickListener(this);
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        buttonFour.setOnClickListener(this);
+        buttonFive.setOnClickListener(this);
+        buttonSix.setOnClickListener(this);
+        buttonSeven.setOnClickListener(this);
+        buttonEight.setOnClickListener(this);
+        buttonNine.setOnClickListener(this);
+        buttonZero.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        buttonPoint.setOnClickListener(this);
+        buttonDivision.setOnClickListener(this);
+        buttonMultiplication.setOnClickListener(this);
+        buttonCount.setOnClickListener(this);
+    }
+
+    private void initView() {
+        buttonOne = findViewById(R.id.button_one);
+        buttonTwo = findViewById(R.id.button_two);
+        buttonThree = findViewById(R.id.button_three);
+        buttonFour = findViewById(R.id.button_four);
+        buttonFive = findViewById(R.id.button_five);
+        buttonSix = findViewById(R.id.button_six);
+        buttonSeven = findViewById(R.id.button_seven);
+        buttonEight = findViewById(R.id.button_eight);
+        buttonNine = findViewById(R.id.button_nine);
+        buttonZero = findViewById(R.id.button_zero);
+        buttonPlus = findViewById(R.id.button_plus);
+        buttonMinus = findViewById(R.id.button_minus);
+        buttonPoint = findViewById(R.id.button_point);
+        buttonMultiplication = findViewById(R.id.button_multiplication);
+        buttonDivision = findViewById(R.id.button_division);
+        buttonCount = findViewById(R.id.button_count);
+
+        calculatorTextView = findViewById(R.id.calculator_screen);
     }
 }
